e05547a6bd6d7e88d48d85b9c59020590dc7b361
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-09 16:56:13 -0800

    Update Program 2.cpp

--- Program 2.cpp
+++ Program 2.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 using namespace std;
 
+int thing(string s) {
+    string thing1 = " ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    string thing2 = " abcdefghijklmnopqrstuvwxyz ";
+    int end = 0;
+    int j;
+    for (j = 0; s[j]; j++);//I totaly stole this line off the internet.
+    //It tells me how long s is.
+
+    for (int i = 0; i < j; i++) {
+        for (int k = 1; k < 28; k++) {
+            if (s[i] == thing1[k] || s[i] == thing2[k]) {
+                end = end + k;
+            }
+        }
+    }
+
+
+    return end % 31;
+}
+
+int main() {
+    cout << "Hash 31 program start." << endl;
+    bool stillGoing = true;
+    int cont;
+    string holder;
+    while(true) {
+        cout << "\nPlease enter a sentence using only letters and spaces: \n";
+        getline(cin, holder);
+        //cin >> holder;
+        cout << thing(holder) << endl << endl;
+    }
+
+    return 17;
+}
