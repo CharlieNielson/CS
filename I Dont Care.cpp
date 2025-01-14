7ae06cd95a37d14dad9e1e1a7ba2e827222884f1
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-14 15:10:10 -0700

    Update I Dont Care.cpp

--- I Dont Care.cpp
+++ I Dont Care.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int main() {//Calculates c for an ISBN number. I only had to calculate about four of these, but read the title of the program if you wonder why.
+    int calc = 0;
+    int end = 0;
+    cout << "Put in the ISBN: ";
+    cin >> calc;
+    for (int i = 1; i <= 9; i++) {
+        end += (calc % 10) * (10 - i);
+        calc -= (calc % 10);
+        calc = calc / 10;
+    }
+    end = end % 11;
+    cout << endl << "C: " << end << endl;
+
+    return 17;
+}
