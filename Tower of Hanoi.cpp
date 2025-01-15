55a353834c69843157027ba7ca587de658119829
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-14 17:10:51 -0800

    Update Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
 
-string move(int take, int place) {
+void move(int &take, int &place, int size, int &peg1, int &peg2, int &peg3) {//Moves the pieces.
+    int piece = 0;
+    string end;
+
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        if (take[i] != 0) {
+            piece = take[i];
+        }
+    }
+    take[i] = 0;
     
+    i = 0;
+    for (i; i < size; i++) {
+        if (place[i] == 0) {
+            place [i] = piece;
+            break;
+        }
+    }
 
+    for (i = 0; i < size; i++) {
+        end += peg1[i] + " " + peg2[i] + " " + peg3[i] + "\n";
+    }
 
-    return "WIP";
+    cout << end;
 }
 
 void solve(int start, int end, int n) {//Recursive function that solves a tower of hanoi of size n from start peg to end peg.
