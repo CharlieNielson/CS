1cb2b5ae88facaeec1a0e81767a03b944b6f9f8f
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-14 16:16:01 -0800

    Update Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
-useing namespace std;
+using namespace std;
 
+string move(int take, int place) {
+    
+
+
+    return "WIP";
+}
+
+void solve(int start, int end, int n) {//Recursive function that solves a tower of hanoi of size n from start peg to end peg.
+
+}
+
+int main() {
+    int size;
+    cout << "How large is the tower: ";
+    cin >> size;
+    cout << endl;
+    int peg1[size];
+    int peg2[size];
+    int peg3[size];
+    for (int i = 0; i < size; i++) {
+        peg1[i] = i + 1;
+    }
+
+
+    return 17;
+}
