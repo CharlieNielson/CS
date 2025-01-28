94ee4f0a11f4bcede5c9831723c0ac1c1005126d
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-28 16:09:09 -0700

    Update Program 3.cpp

--- Program 3.cpp
+++ Program 3.cpp
@@ -1,9 +1,26 @@
-#inlcude <iostream>
+#include <iostream>
 using namespace std;
+
+string iterate(int n) {
+    string end = "";
+    for (int i = 0; i < n; i++) {
+        end = end + "a";
+    }
+    for (int i = 0; i < n; i++) {
+        end = end + "b";
+    }
+    return end;
+    // I originaly meant for this function to call itself, but I 
+    // realized that without if statements, it would loop forever.
+}
 
 
 int main() {
 
+    for (int i = 5; i > 0; i--) {
+        cout << iterate(i);
+    }
+    cout << endl;
 
     return 17;
 }
