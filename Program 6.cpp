8548aa7972426e33e3b7e54d7af99e26cc4c3302
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-28 19:51:03 -0700

    Update Program 6.cpp

--- Program 6.cpp
+++ Program 6.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 
 int main() {
-    cout << "p q | ~p A q" << endl;
-
+    cout << "a b c d e | (( c V ~d ) A b ) A ~( d V a A e )" << endl;
+    cout << "----------------------------------------------" << endl;
     int end;
-    for (int p = 0; p < 2; p++) {
-        for (int q = 0; q < 2; q++) {
-            if ((p == 0) && (q == 1)) {
-                end = 1;
-            } else {
-                end = 0;
+    for (int a = 0; a < 2; a++) {
+        for (int b = 0; b < 2; b++) {
+            for (int c = 0; c < 2; c++) {
+                for (int d = 0; d < 2; d++) {
+                    for (int e = 0; e < 2; e++) {
+                        if ((((c == 1) || (d == 0)) && (b == 1)) && (((d == 1) || (a == 1) && (e == 1)) == false)) {
+                            end = 1;
+                        } else {
+                            end = 0;
+                        }
+                        cout << a << " "  << b << " "  << c << " "  << d << " "  << e << " |                   " << end << endl;
+                    }
+                }
             }
-            cout << p << " " << q << " |    " << end << endl;
         }
     }
 
