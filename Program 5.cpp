8fc7a385a5175d19dc47fdbac64cbcac500f62b3
CharlieNielson <charlienielson@mail.weber.edu>
2025-01-28 19:27:28 -0700

    Update Program 5.cpp

--- Program 5.cpp
+++ Program 5.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 
 int main() {
+    cout << "p q | ~p A q" << endl;
 
+    int end;
+    for (int p = 0; p < 2; p++) {
+        for (int q = 0; q < 2; q++) {
+            if ((p == 0) && (q == 1)) {
+                end = 1;
+            } else {
+                end = 0;
+            }
+            cout << p << " " << q << " |    " << end << endl;
+        }
+    }
 
     return 17;
 }
