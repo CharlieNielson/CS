bf585bd1670a027b2a8b9f67e045b1cf6f48d316
CharlieNielson <charlienielson@mail.weber.edu>
2025-02-20 13:06:37 -0700

    Update Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -1,55 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void move(int &take, int &place, int size, int &peg1, int &peg2, int &peg3) {//Moves the pieces.
-    int piece = 0;
-    string end;
-
-    int i = 0;
-    for (i = 0; i < size; i++) {
-        if (take[i] != 0) {
-            piece = take[i];
-        }
-    }
-    take[i] = 0;
-    
-    i = 0;
-    for (i; i < size; i++) {
-        if (place[i] == 0) {
-            place [i] = piece;
-            break;
-        }
-    }
-
-    for (i = 0; i < size; i++) {
-        end += peg1[i] + " " + peg2[i] + " " + peg3[i] + "\n";
-    }
-
-    cout << end;
-}
-
-void solve(int start, int end, int n) {//Recursive function that solves a tower of hanoi of size n from start peg to end peg.
+void show() {// Outputs the board.
 
 }
+
+void move(int start, int end, int blocks[], int size) {// Moves the pieces.
+    bool notEnd = true;
+    for (int i = 0; i < size && notEnd; i++) {
+        if (blocks[i] == start) {
+            blocks[i] = end;
+            notEnd = false;
+        }
+    }
+}
+
+void solve(int start, int other, int end, int size, int blocks[]) {// Recursive function that solves a tower of hanoi of size size.
+    if (size == 1) {
+        move(start, end, size);
+    }
+}
+
 
 int main() {
-    int size;
+    int size;// The number of blocks.
     cout << "How large is the tower: ";
     cin >> size;
-    cout << endl;
-    int peg1[size];
-    int peg2[size];
-    int peg3[size];
-    for (int i = 0; i < size; i++) {
-        peg1[i] = i + 1;
+    int blocks[size];// Either one, two, or three, each element stores the location of a block.
+    for (int i = 0; i < size; i++) {// Sets each blocks position to one. They will only ever be 1, 2, or 3;
+        blocks[i] = 1;
     }
-    for (int i = 0; i < size; i++) {
-        peg2[i] = 0;
-    }
-    for (int i = 0; i < size; i++) {
-        peg3[i] = 0;
-    }
+    solve(1, 2, 3, size, blocks[]);
 
-
-    return 17;
+    return 0;
 }
