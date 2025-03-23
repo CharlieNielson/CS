ec44fa6be98e4305dfe9611d40c44cdbf6c42c41
CharlieNielson <charlienielson@mail.weber.edu>
2025-03-22 19:14:05 -0600

    Update Program 9.cpp

--- Program 9.cpp
+++ Program 9.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void display(int matrix[]) {
+    //WIP
+}
+
 
 int main() {
-
+    int matrix1[2][2] = {{9, 4}, {2, 5}};//2X2 matrix
+    int matrix2[2][3] = {{9, 5, 1}, {8, 5, 10}};//2X3 matrix
+    int matrix3[3][2] = {{10, 7}, {10, 4}, {3, 6}};//3X2 matrix
+    int matrix4[3][3] = {{5, 9, 2}, {6, 4, 1}, {1, 9, 2}};//3X3 matrix
 
     
 
-    return 0
+    return 0;
 }
