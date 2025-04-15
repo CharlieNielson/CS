c509fa3ece161b1dc785141868adfd43539cc690
CharlieNielson <charlienielson@mail.weber.edu>
2025-04-14 20:32:05 -0600

    Update Program 12.cpp

--- Program 12.cpp
+++ Program 12.cpp
@@ -4,77 +4,88 @@
 using namespace std;
 
 struct Node {
-  Node(int data): data(data), left(nullptr), right(nullptr) {}
-  int data;
-  shared_ptr<Node<int>> left;
-  shared_ptr<Node<int>> right;
+    Node(int data): data(data), left(nullptr), right(nullptr) {}
+    int data;
+    shared_ptr<Node> left;
+    shared_ptr<Node> right;
 };
 
 class BTree {
 public:
-  BTree(): root(nullptr) {}
-  BTree(const BTree<int>&);
-  BTree<int>& operator=(const BTree<int>&);
-  void insert(const int& item);
-  void preOrder();
-  void inOrder();
-  void postOrder();
+    BTree(): root(nullptr) {}
+    BTree(const BTree&);
+    BTree& operator=(const BTree&);
+    void insert(const int& item);
+    void preOrder();
+    void inOrder();
+    void postOrder();
 
 private:
-  void preOrder(shared_ptr<Node<int>> ptr);
-  void inOrder(shared_ptr<Node<int>> ptr);
-  void postOrder(shared_ptr<Node<int>> ptr);
-  void insert(const int& item, shared_ptr<Node<int>> ptr
+    void preOrder(shared_ptr<Node> ptr);
+    void inOrder(shared_ptr<Node> ptr);
+    void postOrder(shared_ptr<Node> ptr);
+    void insert(const int& item, shared_ptr<Node> ptr);
+    shared_ptr<Node> root;
 };
 
-void BTree<int>::insert(const int& item, shared_ptr<Node<int>> ptr) {
-  if (item < ptr->data) {
-    if (ptr->left == nullptr) {
-      ptr->left = make_shared<Node<int>>(item);
+void BTree::insert(const int& item, shared_ptr<Node> ptr) {
+    if (item < ptr->data) {
+        if (ptr->left == nullptr) {
+            ptr->left = make_shared<Node>(item);
+        } else {
+            insert(item, ptr->left);
+        }
     } else {
-      insert(item, ptr->left);
+        if (ptr->right == nullptr) {
+            ptr->right = make_shared<Node>(item);
+        } else {
+            insert(item, ptr->right);
+        }
     }
-  } else {
-    if (ptr->right == nullptr) {
-      ptr->right = make_shared<Node<int>>(item);
-    } else {
-      insert(item, ptr->right);
+}
+
+void BTree::preOrder() {
+    preOrder(root);
+}
+
+void BTree::preOrder(shared_ptr<Node> ptr) {
+    if (ptr) {
+        cout << ptr->data << " ";
+        preOrder(ptr->left);
+        preOrder(ptr->right);
     }
-  }
 }
 
-void BTree<int>::preOrder() {
-  preOrder(root);
+void BTree::inOrder() {
+    inOrder(root);
 }
 
-void BTree<int>::preOrder(shared_ptr<Node<int>> ptr) {
-  if (ptr) {
-    cout << ptr->data << " ";
-    preOrder(ptr->left);
-    preOrder(ptr->right);
-  }
+void BTree::inOrder(shared_ptr<Node> ptr) {
+    if (ptr) {
+        preOrder(ptr->left);
+        cout << ptr->data << " ";
+        preOrder(ptr->right);
+    }
 }
 
-void BTree<int>::inOrder() {
-  inOrder(root);
+void BTree::postOrder() {
+    postOrder(root);
 }
 
-void BTree<int>::inOrder(shared_ptr<Node<int>> ptr) {
-  if (ptr) {
-    preOrder(ptr->left);
-    cout << ptr->data << " ";
-    preOrder(ptr->right);
-  }
+void BTree::postOrder(shared_ptr<Node> ptr) {
+    if (ptr) {
+        preOrder(ptr->left);
+        preOrder(ptr->right);
+        cout << ptr->data << " ";
+    }
 }
 
-void BTree<int>::postOrder() {
-  postOrder(root);
-}
 
-void BTree<int>::postOrder(shared_ptr<Node<int>> ptr) {
-  if (ptr) {
-    preOrder(ptr->left);
-    preOrder(ptr->right);
-    cout << ptr->data << " ";
-  }
+
+int main() {
+    
+
+
+
+    return 0;
 }
